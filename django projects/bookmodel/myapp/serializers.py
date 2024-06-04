f18e2e69ed39910c96eaf1e90a3c16137049c345
Nirajuginoya <nirajuginoya@gmail.com>
2024-06-04 14:10:53 +0530

    first commit

---
+++ django projects/bookmodel/myapp/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import *
+
+class bookserializer(serializers.ModelSerializer):
+    class Meta:
+        model=booklist
+        fields='__all__'
